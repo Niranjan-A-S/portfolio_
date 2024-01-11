d900ff47eb2f29d2b6de842d8c1165997f85e96a
Niranjan-A-S <niranjan0881@gmail.com>
2024-01-11 19:31:31 +0530

    chore: completed project

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,7 +1,8 @@
 import { FC, memo, useCallback } from 'react';
 
 export const Sidebar: FC = memo(() => {
-    const renderLineNumbers = useCallback(() => Array(50).fill(0).map((_, index) => <li key={index} className='line-number'>{index + 1}</li>), []);
+    //TODO: Need to generate the numbers dynamically
+    const renderLineNumbers = useCallback(() => Array(130).fill(0).map((_, index) => <li key={index} className='line-number'>{index + 1}</li>), []);
 
     return (
         <div className='sidebar'>
